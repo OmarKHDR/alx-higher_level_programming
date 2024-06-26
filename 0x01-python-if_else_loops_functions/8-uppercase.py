1a2fb881072e7a0c21d549d3983da0b592e0ee47
OmarKHDR <omarsliman37@gmail.com>
2024-06-26 11:47:39 +0300

    adding 8-uppercase.py

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -2,8 +2,7 @@
 
 def uppercase(str1):
     k = ord('a')-ord('A')
-    a=ord('a')
-    z=ord('z')
+    a = ord('a')
+    z = ord('z')
     for i in range(len(str1)):
-        print("{0}".format((str1[i],chr(ord(str1[i])-k))[ord(str1[i]) >= a and ord(str1[i]) <= z]),end=("","\n")[i == len(str1) - 1])
-uppercase("hello world!")
+        print("{0}".format((str1[i], chr(ord(str1[i]) - k))[ord(str1[i]) >= a and ord(str1[i]) <= z]), end=("", "\n")[i == len(str1) - 1])
