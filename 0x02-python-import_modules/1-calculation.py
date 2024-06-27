e62b19713711027965b19683054423804041e463
OmarKHDR <omarsliman37@gmail.com>
2024-06-27 14:29:48 +0300

    adding 1-calculation.py

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+if __name__ == '__main__':
+    import calculator_1 as cal
+    a = 10
+    b = 5
+    print("{} + {} = {}".format(a, b, cal.add(a, b)))
+    print("{} + {} = {}".format(a, b, cal.sub(a, b)))
+    print("{} + {} = {}".format(a, b, cal.mul(a, b)))
+    print("{} + {} = {}".format(a, b, cal.div(a, b)))
+
+"""
+10 + 5 = 15
+10 - 5 = 5
+10 * 5 = 50
+10 / 5 = 2
+"""
