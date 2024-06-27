bc67a6adf68fc0691fa0452e469d036429a2e4fb
OmarKHDR <omarsliman37@gmail.com>
2024-06-27 14:05:08 +0300

    adding 0-add.py

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,5 +1,7 @@
-from add_0 import add as add
+import add_0 as add
 a = 1
 b = 2
-if __name__ == '__main__':
-    print("1 + 2 = {}".format(add(a, b)))
+if __name__ == "__main__":
+    print("1 + 2 = {}".format(add.add(a, b)))
+else:
+    print("no")
