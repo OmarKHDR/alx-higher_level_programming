c2e848f7f227ac5f87924d3b6ec7c9d079befa6d
OmarKHDR <omarsliman37@gmail.com>
2024-06-30 11:40:49 +0300

    adding 7-add_tuple.py

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 
 def add_tuple(tuple_a=(), tuple_b=()):
-    return tuple([x + y for x, y in zip(tuple_a, tuple_b)])
+    a = tuple_a + (0,) * (2 - len(tuple_a))
+    b = tuple_b + (0,) * (2 - len(tuple_b))   
+    return tuple([x + y for x, y in zip(a, b)])
