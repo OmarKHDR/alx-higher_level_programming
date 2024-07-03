722c0f80fef5404d5ac8f779445dbba3f513d722
OmarKHDR <omarsliman37@gmail.com>
2024-07-03 23:17:50 +0300

    Dv

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,70 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-size_t print_listint(const listint_t *h)
-{
-    const listint_t *current;
-    unsigned int n; /* number of nodes */
-
-    current = h;
-    n = 0;
-    while (current != NULL)
-    {
-        printf("%i\n", current->n);
-        current = current->next;
-        n++;
-    }
-
-    return (n);
-}
-
-/**
- * add_nodeint_end - adds a new node at the end of a listint_t list
- * @head: pointer to pointer of first node of listint_t list
- * @n: integer to be included in new node
- * Return: address of the new element or NULL if it fails
- */
-listint_t *add_nodeint_end(listint_t **head, const int n)
-{
-    listint_t *new;
-    listint_t *current;
-
-    current = *head;
-
-    new = malloc(sizeof(listint_t));
-    if (new == NULL)
-        return (NULL);
-
-    new->n = n;
-    new->next = NULL;
-
-    if (*head == NULL)
-        *head = new;
-    else
-    {
-        while (current->next != NULL)
-            current = current->next;
-        current->next = new;
-    }
-
-    return (new);
-}
-
-/**
- * free_listint - frees a listint_t list
- * @head: pointer to list to be freed
- * Return: void
- */
-void free_listint(listint_t *head)
-{
-    listint_t *current;
-
-    while (head != NULL)
-    {
-        current = head;
-        head = head->next;
-        free(current);
-    }
-}
+#include "lists.h"
 listint_t *reverse(listint_t *head);
 
 int is_palindrome(listint_t **head)
@@ -91,7 +27,7 @@ int is_palindrome(listint_t **head)
         end = end->next;
     }
 
-    begin = end->next ;
+    begin = (count % 2 == 0)? end : end->next ;
     end = *head;
 
     begin = reverse(begin);
