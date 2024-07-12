96e737cc8b23d806ba5f4f8e5696ee17ffc59c79
OmarKHDR <omarsliman37@gmail.com>
2024-07-12 20:02:15 +0300

    adding 1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Making a new class called Square"""
+
+
+class Square:
+    """THe SUMmary summary
+    Attributes:
+    """
+    def __init__(self, size=0) -> None:
+        """THe SUMmary summary
+        Args: size = 0
+        """
+        self.__size = size
+    
