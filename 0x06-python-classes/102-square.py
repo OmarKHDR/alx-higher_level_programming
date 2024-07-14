3a6e2504fb6990215d5dc7d8566f55522044ad52
OmarKHDR <omarsliman37@gmail.com>
2024-07-14 06:24:12 +0300

    adding 102-square.py

---
+++ 0x06-python-classes/102-square.py
@@ -0,0 +1,81 @@
+#!/usr/bin/python3
+"""Making a new class called Square"""
+
+
+class Square:
+    """THe SUMmary summary    """
+    def __init__(self, size=0) -> None:
+        """THe SUMmary summary
+
+        Args:
+            self
+            size = 0
+        """
+        self.size = size
+
+    @property
+    def size(self):
+        """THe SUMmary summary
+
+        Args:
+            self
+        """
+        return self._size
+
+    @size.setter
+    def size(self, s):
+        """THe SUMmary summary
+
+        Args:
+            self
+            s = 0
+        """
+        if type(s) is not int:
+            raise TypeError("size must be integer")
+        if s < 0:
+            raise ValueError("size must be >= 0")
+
+        self._size = s
+
+    def area(self):
+        """THe SUMmary summary
+
+        Args:
+            self
+        """
+        return self._size ** 2
+
+    def __eq__(self, o) -> bool:
+        if self._size == o.size:
+            return True
+        else:
+            return False
+    def __ne__(self, o) -> bool:
+        if self._size != o.size:
+            return True
+        else:
+            return False
+    
+    def __gt__(self,o):
+        if self._size > o.size:
+            return True
+        else:
+            return False
+    
+    def __lt__(self, o):
+        if self._size < o.size:
+            return True
+        else:
+            return False
+        
+    def __ge__(self,o):
+        if self._size >= o.size:
+            return True
+        else:
+            return False
+    
+    def __le__(self, o):
+        if self._size <= o.size:
+            return True
+        else:
+            return False
