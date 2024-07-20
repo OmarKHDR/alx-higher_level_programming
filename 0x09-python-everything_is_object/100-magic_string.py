3c6b7b876bf1bed656d298221abac7dfc8c8bbfd
OmarKHDR <omarsliman37@gmail.com>
2024-07-20 05:49:48 +0300

    adding 101-locked_class.py

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-
+""" this file is no good"""
 counter = 0
 
 
 def magic_string():
-    """
+    """ class of strings
         arg:
         None,
         return empty string
