962aee10b2d5c97fcd99326252b467023690ffb5
OmarKHDR <omarsliman37@gmail.com>
2024-07-20 05:25:03 +0300

    adding 100-magic_string.py

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -2,10 +2,15 @@
 
 counter = 0
 
+
 def magic_string():
+    """
+        arg:
+        None,
+        return empty string
+    """
     global counter
     counter += 1
     for i in range(counter):
-        print("BestSchool", end=("",", ")[i < counter - 1])
+        print("BestSchool", end=("", ", ")[i < counter - 1])
     return ""
-
