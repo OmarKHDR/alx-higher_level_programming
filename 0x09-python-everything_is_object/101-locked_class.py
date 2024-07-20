5d430792718fa00a792c59cb4602233441d0ea05
OmarKHDR <omarsliman37@gmail.com>
2024-07-20 05:49:00 +0300

    adding 101-locked_class.py

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""Here is my class and nothing speical about it"""
+
 
 class LockedClass:
+    """Here is my class and nothing speical about it"""
     __slots__ = ("first_name",)
