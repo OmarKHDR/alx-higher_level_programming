0066e80810409ee479eb5c4e5add0e40a78b6a24
OmarKHDR <omarsliman37@gmail.com>
2024-07-21 07:27:41 +0300

    adding 4-inherits_from.py

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+"""is instances???????"""
+
+
+def is_kind_of_class(obj, a_class):
+    """ s*h*itty func
+        args: yes it is
+    """
+    return isinstance(obj, a_class) and not (type(obj) is a_class)
