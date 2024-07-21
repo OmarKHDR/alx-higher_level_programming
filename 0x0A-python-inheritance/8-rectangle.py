0c7be6c656320fdd24ea779859b44d93ccf98395
OmarKHDR <omarsliman37@gmail.com>
2024-07-21 07:55:49 +0300

    adding 8-rectangle.py

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+
+""" file is not alright and and so am i"""
+
+
+class BaseGeometry:
+    """classes
+    """
+    def area(self):
+        """ area
+            args:
+        """
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """ valid
+            args : name, value
+        """
+        if type(value) is not int:
+            raise TypeError("{:s} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{:s} must be greater than 0".format(name))
+
+
+class Rectangle(BaseGeometry):
+    """ Rect
+    """
+    def __init__(self, width, height):
+        """ initialization
+            args: width and height
+        """
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
