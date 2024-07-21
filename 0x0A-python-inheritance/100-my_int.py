cdf822eeb5ee853754693338c67e7e884ffecddf
OmarKHDR <omarsliman37@gmail.com>
2024-07-21 10:06:23 +0300

    adding 100-my_int.py

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -3,11 +3,24 @@
 
 
 class MyInt(int):
+    """ class with ducked up methods"""
+
     def __init__(self, val) -> None:
+        """init
+            args: val
+        """
         self.val = val
 
     def __eq__(self, obj) -> bool:
+        """ ==
+            args:
+            obj
+        """
         return self.val != obj
 
     def __ne__(self, obj) -> bool:
+        """ !=
+            args:
+            obj
+        """
         return obj == self.val
