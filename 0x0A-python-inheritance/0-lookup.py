85384b5ccb1df888e7c680be0d28558e5608f44f
OmarKHDR <omarsliman37@gmail.com>
2024-07-21 06:29:58 +0300

    adding 0-lookup.py

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -8,4 +8,17 @@ def lookup(obj):
         args:
             obj
     """
-    return obj.__dict__
+    return list(type(obj).__dict__)
+
+
+class MyClass1(object):
+    pass
+
+class MyClass2(object):
+    my_attr1 = 3
+    def my_meth(self):
+        pass
+
+print(lookup(MyClass1))
+print(lookup(MyClass2))
+print(lookup(int))
