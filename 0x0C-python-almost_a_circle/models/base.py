5803c2a20e80807c8fad3e1166b0082208e6ce7f
OmarKHDR <omarsliman37@gmail.com>
2024-07-22 07:07:55 +0300

    adding rectangle.py

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -3,9 +3,11 @@
 
 
 class Base:
+    """my class"""
     __nb_objects = 0
 
     def __init__(self, id=None) -> None:
+        """yeah oop"""
         if id == None:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
