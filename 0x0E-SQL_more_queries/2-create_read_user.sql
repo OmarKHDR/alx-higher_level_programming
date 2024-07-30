de7abe7b68069019659b5d7ac10b665a9342866b
OmarKHDR <omarsliman37@gmail.com>
2024-07-30 17:45:17 +0300

    TO NOT FOR

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -2,5 +2,5 @@
 
 CREATE DATABASE if NOT EXISTS hbtn_0d_2;
 CREATE USER if NOT EXISTS user_0d_2@localhost;
-GRANT SELECT ON hbtn_0d_2.* FOR user_0d_2@localhost;
+GRANT SELECT ON hbtn_0d_2.* TO user_0d_2@localhost;
 FLUSH PRIVILEGES;
