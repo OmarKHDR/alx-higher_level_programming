4e1580a8d46cf8c69caed8f147ff9d491741c70a
OmarKHDR <omarsliman37@gmail.com>
2024-07-30 17:33:36 +0300

    NOT HAHAHA

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,5 +1,5 @@
 -- -- 
 
-CREATE USER if EXISTS user_0d_1@localhost IDENTIFIED BY 'user_0d_1_pwd';
+CREATE USER if NOT EXISTS user_0d_1@localhost IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES ON *.* TO user_0d_1@localhost;
 FLUSH PRIVILEGES;
