117be39d425720e6c594cc8b84dc2cf587ee52fd
OmarKHDR <omarsliman37@gmail.com>
2024-08-04 16:11:21 +0300

    asdf

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+const myVar = process.argv.length
+
+switch (myVar) {
+    case 2:
+        console.log('No argument');
+        break;
+    
+    case 3:
+        console.log('Argument found');
+        break;
+    default:
+        console.log('Arguments found');
+}
