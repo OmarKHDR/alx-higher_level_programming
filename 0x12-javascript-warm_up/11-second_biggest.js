a9332062a635f66d0eee29177fe792b483bd60d8
OmarKHDR <omarsliman37@gmail.com>
2024-08-05 08:01:28 +0300

    11.11

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+const arr = process.argv
+let frst = 0;
+let scnd = 0;
+
+for (i = 2; i < arr.length; i++) {
+    if (frst < arr[i]) {
+        scnd = frst;
+        frst = parseInt(arr[i])
+    }
+}
+console.log(scnd);
