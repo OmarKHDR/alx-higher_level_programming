2b0c3a2e5f3d0b56053bd4a66612c5d0f11a2506
OmarKHDR <omarsliman37@gmail.com>
2024-08-05 15:32:21 +0300

    please

--- 0x12-javascript-warm_up/main.js
+++ 0x12-javascript-warm_up/main.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-myVar = 40;
-console.log(myVar);
-require('./100-let_me_const')
-console.log(myVar);
+const callMeMoby = require('./101-call_me_moby').callMeMoby;
+callMeMoby(3, function () {
+  console.log('C is fun');
+});
