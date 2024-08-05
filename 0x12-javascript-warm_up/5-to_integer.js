9748f0ed252416c13d6fa13f0581a85d61202c93
OmarKHDR <omarsliman37@gmail.com>
2024-08-05 06:37:29 +0300

    5.2

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -2,11 +2,9 @@
 
 let arg = process.argv[2];
 arg = parseInt(arg);
-if (Number.isInteger(arg))
-{
-    console.log(arg);
+if (Number.isInteger(arg)){
+  console.log(arg);
 }
-else
-{
-    console.log("Not a number");
+else{
+  console.log("Not a number");
 }
