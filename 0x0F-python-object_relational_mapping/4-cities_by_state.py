7b7fd5ec54c8ee62a0f909e147d86c22fa2b5662
OmarKHDR <omarsliman37@gmail.com>
2024-08-07 18:05:30 +0300

    p:S

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" python is so lame
+
+"""
+import MySQLdb
+import sys
+
+
+if __name__ == '__main__':
+    args = sys.argv
+    db = MySQLdb.connect(host="localhost", port=3306,
+                         user=args[1], passwd=args[2], db=args[3])
+    cr = db.cursor()
+    nm = args[4].strip('\"')
+    query = "SELECT * FROM states INNER JOIN cities ON states.id = cities.state_id ORDER BY cities.id ASC;"
+    num = cr.execute(query, (nm + '%',))
+    row = cr.fetchall()
+    for i in range(num):
+        print("{}".format(row[i]))
+    cr.close()
+    db.close()
