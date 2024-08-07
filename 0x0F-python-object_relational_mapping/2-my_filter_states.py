f9d47b291b5907aa9ea1df42534f388d906c20ed
OmarKHDR <omarsliman37@gmail.com>
2024-08-07 14:44:49 +0300

    please

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" python is so lame
+
+"""
+import MySQLdb
+import sys
+
+
+if __name__ == '__main__':
+    args = sys.argv
+    db = MySQLdb.connect(host="localhost", port=3306,
+                         user=args[1], passwd=args[2], db=args[3])
+    cr = db.cursor()
+    query = "SELECT * FROM states WHERE name = args[4] ORDER BY id ASC;"
+    num = cr.execute(query)
+    row = cr.fetchall()
+    for i in range(num):
+        print("{}".format(row[i]))
+    cr.close()
+    db.close()
