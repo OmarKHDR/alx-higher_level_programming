84e95ce633334592392c2c5b2ff56eaccef0792e
OmarKHDR <omarsliman37@gmail.com>
2024-08-07 06:48:34 +0300

    PLEASE

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,16 @@
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    let str = `${'X'.repeat(this.width) + '\n'}`.repeat(this.height);
+    str = str.slice(0,-1);
+    console.log(str);
+  }
+}
+
+module.exports = Rectangle;
