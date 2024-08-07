1a068f886884dd39f7cf288814044291f99efe1a
OmarKHDR <omarsliman37@gmail.com>
2024-08-07 07:49:00 +0300

    ffffffffffff

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -3,12 +3,10 @@
 const square = require('./5-square');
 
 class Square extends square {
-  constructor (size) {
-    super(size);
-  }
+
   charPrint (c = 'X') {
     super.print(c);
-   }
+  }
 }
 
 module.exports = Square;
