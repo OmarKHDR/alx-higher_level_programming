e13b59ed85ff72b619998d910fbf4a889c867747
OmarKHDR <omarsliman37@gmail.com>
2024-08-07 13:53:36 +0300

    fwdq

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 import MySQLdb
 import sys
-
+"""
+    docs is important
+    but idc
+"""
 if __name__ == '__main__':
     args = sys.argv
     db = MySQLdb.connect(host="localhost", port=3306, user=args[1], passwd=args[2], db = args[3])
