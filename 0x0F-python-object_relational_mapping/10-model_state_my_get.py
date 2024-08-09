1ca6f30eb10c92db68045a61ba693312a46ae3cb
OmarKHDR <omarsliman37@gmail.com>
2024-08-09 09:19:52 +0300

    rwf

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -11,8 +11,8 @@ if __name__ == "__main__":
         sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
     Session = sessionmaker(bind=engine)
     ses = Session()
-    st = ses.query(State.id).filter(State.name== sys.argv[4]).first()
+    st = ses.query(State).filter(State.name == sys.argv[4]).first()
     if st is None:
         print("Not found")
     else:
-        print("{}: {}".format(st.id, st.name))
+        print("{}: {}".format(st.id))
