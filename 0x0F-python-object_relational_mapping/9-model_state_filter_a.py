562e93e3edfcf6d5be2783ca9fccbe4802fd0efc
OmarKHDR <omarsliman37@gmail.com>
2024-08-09 11:07:55 +0300

    l

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -11,7 +11,7 @@ if __name__ == "__main__":
         sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
     Session = sessionmaker(bind=engine)
     session = Session()
-    st = session.query(State).order_by(State.id).filter(State.name.like ("%a%"))
+    st = session.query(State).order_by(State.id).filter(State.name.like("%a%"))
     for s in st:
         if s is None:
             print("Nothing")
