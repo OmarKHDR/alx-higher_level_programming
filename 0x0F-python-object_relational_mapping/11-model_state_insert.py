1c27e09e72f549b256edfb95d731b3951688612f
OmarKHDR <omarsliman37@gmail.com>
2024-08-09 09:17:37 +0300

    now >?

---
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""more comments cafew"""
+
+import sys
+from sqlalchemy import Integer, String, Nullable
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+    ses = Session()
+    obj = State(name='Louisiana')
+    st = ses.query(State).order_by(State.id).all()
+    for st in ses:
+        print("{}: {}".format(st.id, st.name))
