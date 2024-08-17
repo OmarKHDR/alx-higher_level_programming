41a126207365a0325c789ff465dd3f7659a5baa2
OmarKHDR <omarsliman37@gmail.com>
2024-08-17 10:28:01 +0300

    p[spld

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # -__=
-curl -s -I $1 | grep 'Allow: ' | tr -d 'Allow: '
+curl -s -X GET 0.0.0.0:5000/route_4 -I | grep 'Allow: ' | sed "s/Allow: //"
--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # --+--
-curl -X GET $1 -H "X-School-User-Id:98"
+curl -s -X GET $1 -H "X-School-User-Id:98"
