bc1911e8a4ddfce324ee5630413443c8e4bebd07
OmarKHDR <omarsliman37@gmail.com>
2024-08-17 12:05:07 +0300

    oo

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -2,14 +2,19 @@
 """docs is docs"""
 
 
+arr = {}
 def find_peak(list_of_integers):
     """ k is for hello world
     """
     if list_of_integers is None:
         return None
-    peak = list_of_integers[0]
-    for i in list_of_integers:
-        if i > peak:
-            peak = i
-    
-    return peak
+    if list_of_integers in arr.keys():
+        return arr[list_of_integers]
+    else:
+        arr.append(list_of_integers)
+        peak = list_of_integers[0]
+        for i in list_of_integers:
+            if i > peak:
+                peak = i
+        
+        return peak
