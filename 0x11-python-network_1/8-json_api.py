c43e0aa1070c0cfdaea29e5eaceccec56f9f8cec
OmarKHDR <omarsliman37@gmail.com>
2024-08-18 19:36:04 +0300

    cfew

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+import requests
+import sys
+
+
+q = sys.argv[1] if len(sys.argv) > 1 else ""
+res = requests.get("http://0.0.0.0:5000/search_user", data={'q':q})
+try:
+    dic = res.json()
+    if dic:
+        print(f"[{id}] {dic['id']}")
+    else:
+        print("No result")
+except:
+    print("Not a valid JSON")
