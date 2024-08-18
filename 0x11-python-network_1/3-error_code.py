448d6b8fba33399d9a89b2775cc3525880b22ad9
OmarKHDR <omarsliman37@gmail.com>
2024-08-18 18:10:40 +0300

    xcw

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""a shitty place called earth"""
+import sys
+import urllib.request
+import urllib.error
+
+
+if __name__ == '__main__':
+    try:
+        req = urllib.request.Request(sys.argv[1])
+        with urllib.request.urlopen(req) as response:
+            r = response.read()
+            print(r)
+    
+    except urllib.error.HTTPError as e:
+        print("Error code:", e.code)
