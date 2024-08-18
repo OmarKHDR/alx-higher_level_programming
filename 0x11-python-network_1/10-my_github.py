8868d7faac3673d3690c5a770244cf137295c871
OmarKHDR <omarsliman37@gmail.com>
2024-08-18 22:11:40 +0300

    df

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""('|')"""
+import requests
+import sys
+from requests.auth import HTTPBasicAuth
+
+
+base = HTTPBasicAuth(sys.argv[1],sys.argv[2])
+res = requests.get(f"https://api.github.com/users/{sys.argv[1]}", auth=base)
+    
+response = res.json()
+print(response['id'])
