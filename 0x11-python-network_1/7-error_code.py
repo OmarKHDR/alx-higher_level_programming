6bc9cb9359a70cac506bf498e05cdfb6b91bbe00
OmarKHDR <omarsliman37@gmail.com>
2024-08-18 19:13:21 +0300

    dsd

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -7,6 +7,6 @@ import sys
 if __name__ == '__main__':
     res = requests.get(sys.argv[1])
     if res.status_code >= 400:
-        print(res.status_code)
+        print("Error code:", res.status_code)
     else:
-        print(res.content)
+        print(res.content.decode('utf-8'))
