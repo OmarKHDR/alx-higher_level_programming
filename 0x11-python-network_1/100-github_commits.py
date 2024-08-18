337bd591ae8118036b504e726d82cb3f04617eff
OmarKHDR <omarsliman37@gmail.com>
2024-08-18 22:49:38 +0300

    .,

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -8,10 +8,5 @@ if __name__ == '__main__':
     api = f"https://api.github.com/repos/{sys.argv[1]}/{sys.argv[2]}/commits"
     res = requests.get(api)
     res = res.json()
-    i = 0
-    for commit in res:
-        if i < 10:
-            print(f"{commit['sha']}:", commit['commit']['author']['name'])
-            i+=1
-        else:
-            break
+    for commit in res[:10]:
+        print(f"{commit['sha']}:", commit['commit']['author']['name'])
