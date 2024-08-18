d494390543d0d5860df10197005caf20c7ed03b1
OmarKHDR <omarsliman37@gmail.com>
2024-08-18 22:15:05 +0300

    ..

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
-"""docs sucks pls"""
+""" docs sucks pls """
+
 import requests
 import sys
 from requests.auth import HTTPBasicAuth
 
 
-base = HTTPBasicAuth(sys.argv[1],sys.argv[2])
-res = requests.get(f"https://api.github.com/users/{sys.argv[1]}", auth=base)
-    
-response = res.json()
-print(response['id'])
+if __name__ == '__main__':
+    base = HTTPBasicAuth(sys.argv[1],sys.argv[2])
+    res = requests.get(f"https://api.github.com/users/{sys.argv[1]}", auth=base)
+        
+    response = res.json()
+    print(response['id'])
