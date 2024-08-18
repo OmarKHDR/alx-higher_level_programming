43cc6fa7bfcfa40672bdc1a47e168fdb1e501123
OmarKHDR <omarsliman37@gmail.com>
2024-08-18 18:20:45 +0300

    fewf

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+import requests
+
+
+if __name__ == '__main__':
+    response = requests.get("https://alx-intranet.hbtn.io/status")
+    print("Body response:")
+    print("\t- type:", type(response))
+    print("\t- content:", response.content.decode("utf-8"))
