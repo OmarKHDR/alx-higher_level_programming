d80e6a1b17096c5ee2d29034a0b6d9957f5424cf
OmarKHDR <omarsliman37@gmail.com>
2024-08-18 18:26:36 +0300

    33

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""response.headers ll"""
+import requests
+import sys
+
+
+if __name__ == '__main__':
+    res = requests.get(sys.argv[1])
+    print(res.headers['X-Request-Id'])
