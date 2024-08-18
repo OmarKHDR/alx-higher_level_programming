42754e07b8b6214417998ca081a09ac3ff32132a
OmarKHDR <omarsliman37@gmail.com>
2024-08-18 13:46:22 +0300

    ,,,

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -7,6 +7,6 @@ import sys
 
 if __name__ == '__main__':
     req = reqlib.Request(sys.argv[1])
-    with reqlib.urlopen(req) as ans :
+    with reqlib.urlopen(req) as ans:
         r = ans.headers['X-Request-Id']
         print(r)
