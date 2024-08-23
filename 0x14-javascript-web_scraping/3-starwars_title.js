80e22945b4e560852da958f6dca6ebc5106a1f67
OmarKHDR <omarsliman37@gmail.com>
2024-08-23 11:50:12 +0300

    .

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 
 const request = require('request');
-ep = process.argv[2];
-url = `https://swapi-api.alx-tools.com/api/films/${ep}/`;
+const ep = process.argv[2];
+const url = `https://swapi-api.alx-tools.com/api/films/${ep}/`;
 request.get(url, (error, response, body)=>{
     console.log(JSON.parse(body).title)
 });
