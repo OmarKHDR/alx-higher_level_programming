5b953f107e97baf559ccd79bd65a61dae8053d33
OmarKHDR <omarsliman37@gmail.com>
2024-08-23 11:26:16 +0300

    fwef

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+
+const request = require('request')
+
+url = "https://alx-intranet.hbtn.io/status"
+request.get(url, (error, response, body) => {
+    console.log(response.statusCode)
+});
