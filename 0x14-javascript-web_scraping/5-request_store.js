182c9e203cf275d565b3571e767b0c8ef87954a7
OmarKHDR <omarsliman37@gmail.com>
2024-08-24 14:06:45 +0300

    fe

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+const request = require('request');
+const fs = require('fs');
+
+const url = process.argv[2];
+const path = process.argv[3];
+
+request.get(url, (error, response, body) => {
+  if (error) {
+    console.log(error);
+    return;
+  }
+  fs.writeFileSync(path, body, 'utf8');
+});
